36c4f350605709568c0deec22eb4a732bf9656de
Moritz Löchner <momo.loechner@gmail.com>
2023-05-11 10:00:27 +0200

    Social Media icons

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,14 +1,58 @@
 import Link from 'next/link';
+import { BsFacebook, BsInstagram, BsLinkedin, BsYoutube } from 'react-icons/bs';
+import { FaTiktok } from 'react-icons/fa';
 
 export default function Footer() {
   return (
-    <footer className="bg-gray-300 p-3 flex flex-col gap-3 sm:flex-row items-center justify-between">
-      <div className="flex flex-row items-center gap-3 text-gray-800">
-        <Link href="/about">Über uns</Link>
-        <Link href="/imprint">Impressum</Link>
-        <Link href="/privacy">Datenschutz</Link>
+    <footer className="bg-gray-300 p-3">
+      <div className="flex flex-col sm:flex-row w-full max-w-screen-lg mx-auto justify-between items-center gap-3">
+        <div className="flex flex-row items-center gap-3 text-gray-800">
+          <Link href="/about" className="hover:text-accent">
+            Über uns
+          </Link>
+          <Link href="/imprint" className="hover:text-accent">
+            Impressum
+          </Link>
+          <Link href="/privacy" className="hover:text-accent">
+            Datenschutz
+          </Link>
+        </div>
+        <div className="flex flex-row items-center gap-3 text-gray-800">
+          <Link
+            href="https://instagram.com/grafenwert"
+            className="hover:text-accent"
+          >
+            <BsInstagram />
+          </Link>
+          <Link
+            href="https://www.tiktok.com/@grafenwert"
+            className="hover:text-accent"
+          >
+            <FaTiktok />
+          </Link>
+          <Link
+            href="https://www.linkedin.com/company/grafenwert"
+            className="hover:text-accent"
+          >
+            <BsLinkedin />
+          </Link>
+          <Link
+            href="https://www.facebook.com/grafenwert"
+            className="hover:text-accent"
+          >
+            <BsFacebook />
+          </Link>
+          <Link
+            href="https://www.youtube.com/@grafenwert"
+            className="hover:text-accent"
+          >
+            <BsYoutube />
+          </Link>
+        </div>
       </div>
-      <p className="text-gray-600">©2023 Grafenwert Immobilien</p>
+      <p className="text-gray-600 text-center mt-3">
+        ©2023 Grafenwert Immobilien
+      </p>
     </footer>
   );
 }
