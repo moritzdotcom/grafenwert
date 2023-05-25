f3bb06b6e28098e932bfe73ce10c09656de6bf95
Moritz Löchner <momo.loechner@gmail.com>
2023-05-25 10:53:34 +0200

    Improved "Wer wir sind" section

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,10 @@ import ServiceCard from '@/components/serviceCard';
 import Head from 'next/head';
 import Image from 'next/image';
 import heroImage from '../public/images/hero.jpg';
+import environmentIcon from '../assets/icons/environment.png';
+import inheritanceIcon from '../assets/icons/inheritance.png';
+import rentAdjustmentsIcon from '../assets/icons/rentAdjustments.png';
+import stayOrLeaveIcon from '../assets/icons/stayOrLeave.png';
 
 export default function Home() {
   const title = 'Grafenwert Immobilien';
@@ -58,56 +62,118 @@ export default function Home() {
             </div>
           </div>
         </header>
-        <main className="my-9 max-w-screen-lg mx-auto">
-          <h1 className="text-accent text-center text-3xl sm:text-4xl">
-            Unsere Leistungen. Für Sie.
-          </h1>
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-3 p-3 mt-5">
-            <ServiceCard
-              href="/services/brokerage"
-              title="Vermittlung"
-              text="Wir vermitteln Immobilien aller Art. Von der Eigentumswohnung bis hin zum Mehrfamilienhaus. Wir sind Ihr Ansprechpartner, wenn es um die Vermittlung von Immobilien geht."
-              imageUrl="/images/services/brokerage.jpg"
-            />
-            <ServiceCard
-              href="/services/consulting"
-              title="Beratung"
-              text="Wir beraten Sie in allen Fragen rund um Immobilien. Von der Finanzierung bis hin zur Immobilienbewertung. Wir sind Ihr Ansprechpartner, wenn es um die Beratung von Immobilien geht."
-              imageUrl="/images/services/consulting.jpg"
-            />
-            <ServiceCard
-              href="/services/management"
-              title="Hausverwaltung"
-              text="Wir verwalten Immobilien aller Art. Von der Eigentumswohnung bis hin zum Mehrfamilienhaus. Wir sind Ihr Ansprechpartner, wenn es um die Verwaltung von Immobilien geht."
-              imageUrl="/images/services/management.jpg"
-            />
-            <ServiceCard
-              href="/services/financing"
-              title="Finanzierung"
-              text="Wir investieren in Immobilien aller Art. Von der Eigentumswohnung bis hin zum Mehrfamilienhaus. Wir sind Ihr Ansprechpartner, wenn es um die Investition in Immobilien geht."
-              imageUrl="/images/services/financing.jpg"
-            />
+        <main className="my-9">
+          <div className="max-w-screen-lg mx-auto">
+            <h1 className="text-accent text-center text-3xl sm:text-4xl">
+              Unsere Leistungen. Für Sie.
+            </h1>
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-3 p-3 mt-5">
+              <ServiceCard
+                href="/services/brokerage"
+                title="Vermittlung"
+                text="Wir vermitteln Immobilien aller Art. Von der Eigentumswohnung bis hin zum Mehrfamilienhaus. Wir sind Ihr Ansprechpartner, wenn es um die Vermittlung von Immobilien geht."
+                imageUrl="/images/services/brokerage.jpg"
+              />
+              <ServiceCard
+                href="/services/consulting"
+                title="Beratung"
+                text="Wir beraten Sie in allen Fragen rund um Immobilien. Von der Finanzierung bis hin zur Immobilienbewertung. Wir sind Ihr Ansprechpartner, wenn es um die Beratung von Immobilien geht."
+                imageUrl="/images/services/consulting.jpg"
+              />
+              <ServiceCard
+                href="/services/management"
+                title="Hausverwaltung"
+                text="Wir verwalten Immobilien aller Art. Von der Eigentumswohnung bis hin zum Mehrfamilienhaus. Wir sind Ihr Ansprechpartner, wenn es um die Verwaltung von Immobilien geht."
+                imageUrl="/images/services/management.jpg"
+              />
+              <ServiceCard
+                href="/services/financing"
+                title="Finanzierung"
+                text="Wir investieren in Immobilien aller Art. Von der Eigentumswohnung bis hin zum Mehrfamilienhaus. Wir sind Ihr Ansprechpartner, wenn es um die Investition in Immobilien geht."
+                imageUrl="/images/services/financing.jpg"
+              />
+            </div>
           </div>
-          <h1 className="text-accent text-center text-3xl sm:text-4xl my-9">
-            Wer wir sind
-          </h1>
-          <div className="mx-3 text-gray-700 flex flex-col gap-3">
-            <p>
-              Grafenwert Immobilien ist ein Zusammenschluss von Löchner Capital
-              und Sam Zahiri (ehemals Colliers International AG und Graefer
-              Immobilien). Gemeinsam bieten wir Ihnen die bestmögliche Beratung
-              in einer immer komplexer werdenden Immobilienwelt.
-            </p>
-            <ul className="list-disc list-inside">
-              <li>ESG</li>
-              <li>Erbschaft</li>
-              <li>Mietanpassungen</li>
-              <li>Stay or leave Analysen</li>
-            </ul>
-            <p>
-              Dabei unterstützen wir Sie ganzheitlich bei der Vermittlung,
-              Verwaltung und Finanzierung.
-            </p>
+          <div className="bg-gray-200 py-5 my-5">
+            <h1 className="text-accent text-center text-3xl sm:text-4xl mb-9">
+              Wer wir sind
+            </h1>
+            <div className="max-w-screen-lg mx-auto px-3 text-gray-700 flex flex-col">
+              <p>
+                Grafenwert Immobilien ist ein Zusammenschluss von Löchner
+                Capital und Sam Zahiri (ehemals Colliers International AG und
+                Graefer Immobilien). Unser Ziel ist es, Ihnen die bestmögliche
+                Beratung in einer immer komplexer werdenden Immobilienwelt zu
+                bieten. Unser Leistungsspektrum umfasst verschiedene Bereiche,
+                um Sie ganzheitlich bei Ihren Immobilienangelegenheiten zu
+                unterstützen. Hier sind einige unserer Schwerpunkte:
+              </p>
+              <div className="my-3 flex flex-col gap-3">
+                <div className="flex gap-3 sm:gap-5 items-center flex-col sm:flex-row">
+                  <Image src={environmentIcon} alt="ESG" width={70} />
+                  <div>
+                    <h5 className="text-accent text-lg">
+                      ESG (Environmental, Social, and Governance)
+                    </h5>
+                    Wir beraten Sie bei Fragen der Nachhaltigkeit und
+                    unterstützen Sie dabei, umweltfreundliche und sozial
+                    verantwortungsbewusste Immobilieninvestitionen zu tätigen.
+                    Wir helfen Ihnen, nachhaltige Immobilienprojekte zu
+                    identifizieren und umzusetzen.
+                  </div>
+                </div>
+                <div className="flex gap-3 sm:gap-5 items-center flex-col sm:flex-row">
+                  <Image src={inheritanceIcon} alt="Erbschaft" width={70} />
+                  <div>
+                    <h5 className="text-accent text-lg">Erbschaft</h5>
+                    Wenn Sie eine Immobilie erben oder vererben möchten, stehen
+                    wir Ihnen mit unserer Expertise zur Seite. Wir unterstützen
+                    Sie bei der Bewertung der Immobilie, der Klärung rechtlicher
+                    Aspekte und der Durchführung eines reibungslosen
+                    Übergabeprozesses.
+                  </div>
+                </div>
+                <div className="flex gap-3 sm:gap-5 items-center flex-col sm:flex-row">
+                  <Image
+                    src={rentAdjustmentsIcon}
+                    alt="Mietanpassungen"
+                    width={70}
+                  />
+                  <div>
+                    <h5 className="text-accent text-lg">Mietanpassungen</h5>
+                    Bei Mietanpassungen helfen wir Ihnen, angemessene und faire
+                    Mietpreise festzulegen. Wir analysieren den aktuellen
+                    Immobilienmarkt und berücksichtigen lokale Gegebenheiten, um
+                    eine optimale Mietstrategie für Ihre Immobilie zu
+                    entwickeln.
+                  </div>
+                </div>
+                <div className="flex gap-3 sm:gap-5 items-center flex-col sm:flex-row">
+                  <Image src={stayOrLeaveIcon} alt="StayOrLeave" width={70} />
+                  <div>
+                    <h5 className="text-accent text-lg">
+                      Stay or leave Analysen
+                    </h5>
+                    Wenn Sie vor der Entscheidung stehen, eine Immobilie zu
+                    behalten oder zu veräußern, bieten wir Ihnen umfassende
+                    Analysen und Beratung. Wir bewerten die Wertentwicklung
+                    Ihrer Immobilie, berücksichtigen Markttrends und
+                    unterstützen Sie dabei, eine fundierte Entscheidung zu
+                    treffen.
+                  </div>
+                </div>
+              </div>
+              <p>
+                Darüber hinaus stehen wir Ihnen bei der Vermittlung, Verwaltung
+                und Finanzierung von Immobilien zur Seite. Unser Team aus
+                erfahrenen Experten arbeitet eng mit Ihnen zusammen, um
+                maßgeschneiderte Lösungen für Ihre individuellen Anforderungen
+                zu entwickeln. Kontaktieren Sie uns gerne, um weitere
+                Informationen zu erhalten oder um einen Beratungstermin zu
+                vereinbaren. Wir freuen uns darauf, Ihnen bei Ihren
+                Immobilienangelegenheiten behilflich zu sein.
+              </p>
+            </div>
           </div>
         </main>
       </div>
