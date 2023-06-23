23bd240548f4416f1ec6faf902091f6737f77fb4
Moritz Löchner <momo.loechner@gmail.com>
2023-06-23 17:41:36 +0200

    Update phone number and google map

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,7 +1,5 @@
-import GoogleMap from '@/components/googleMap';
 import useQueryParams from '@/hooks/useQueryParams';
 import Head from 'next/head';
-import { GetStaticProps } from 'next/types';
 import { FormEvent, useState } from 'react';
 import { MdLocalPhone, MdMail } from 'react-icons/md';
 
@@ -11,11 +9,7 @@ type ErrorObject = {
   message?: string;
 };
 
-export default function ContactPage({
-  gmapsApiKey,
-}: {
-  gmapsApiKey: string | undefined;
-}) {
+export default function ContactPage() {
   const [loading, setLoading] = useState(false);
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
@@ -102,9 +96,9 @@ export default function ContactPage({
             <p>Grafenwert Immobilien GmbH</p>
             <p>Graf-Adolf-Platz 12</p>
             <p>40213 Düsseldorf</p>
-            <a className="block mt-3" href="tel:+49 152 28688082">
+            <a className="block mt-3" href="tel:+49 211 46827765">
               <MdLocalPhone className="inline text-lg mr-2 text-accent" />
-              0152 286 880 82
+              +49 211 468 277-65
             </a>
             <a className="block mt-0 lg:mt-3" href="mailto:info@grafenwert.de">
               <MdMail className="inline text-lg mr-2 text-accent" />
@@ -205,25 +199,17 @@ export default function ContactPage({
         <h3 className="text-accent mt-10 text-2xl lg:text-3xl font-bold pb-3 border-b border-gray-300">
           So finden Sie uns
         </h3>
-        <div className="w-full h-72">
-          {gmapsApiKey && (
-            <GoogleMap
-              apiKey={gmapsApiKey}
-              marker={{ lat: 51.29608, lng: 6.73654 }}
-            />
-          )}
+        <div className="w-full h-72 sm:h-96">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d156.18866382634994!2d6.778234707676845!3d51.21873262485454!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47b8ca3e7732545d%3A0x63052a55db7c45d7!2sGraf-Adolf-Platz%2012%2C%2040213%20D%C3%BCsseldorf!5e0!3m2!1sde!2sde!4v1687526507156!5m2!1sde!2sde"
+            width="100%"
+            height="100%"
+            style={{ border: 0 }}
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+          ></iframe>
         </div>
       </main>
     </>
   );
 }
-
-export const getStaticProps: GetStaticProps = async () => {
-  const gmapsApiKey = process.env.GMAPS_API_KEY;
-
-  return {
-    props: {
-      gmapsApiKey,
-    },
-  };
-};
